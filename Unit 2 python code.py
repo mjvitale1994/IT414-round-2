9f66da7367294383b756d5743a012801f89ae15f
mjvitale1994 <151416874+mjvitale1994@users.noreply.github.com>
2023-11-19 20:48:02 +0900

    Update Unit 2 python code.py

--- Unit 2 python code.py
+++ Unit 2 python code.py
@@ -2,8 +2,8 @@ def add (x,y):
     result = x+y
     print(x,"+",y,"=",result)
 
-n1 = int(input(1))
-n2 = int(input(2))
+n1 = int(input("Enter first vairiable:"))
+n2 = int(input("Enter second variable:"))
 
 add(n1,n2)
 
