10e295132d5d6cf1b01d12b7003088cd3df1adc3
mjvitale1994 <151416874+mjvitale1994@users.noreply.github.com>
2023-11-21 21:10:01 +0900

    Update Unit 2 python code.py

--- Unit 2 python code.py
+++ Unit 2 python code.py
@@ -1,15 +1,15 @@
 import unittest  # Importing the unittest module for writing and running tests
 
 # Function to add two numbers and return the result
-def add_numbers(num1, num2):
-    return num1 + num2
+def add(x, y):
+    return x + y
 
 # Main function to prompt user for input and display the result of addition
 def main():
-    num1 = int(input("Enter first number: "))  # Prompting user for the first number as an integer
-    num2 = int(input("Enter second number: "))  # Prompting user for the second number as an integer
+    x = int(input("Enter first varirable: "))  # Prompting user for the first number as an integer
+    y = int(input("Enter second variable: "))  # Prompting user for the second number as an integer
 
-    result = add_numbers(num1, num2)  # Calling the add_numbers function with user inputs
+    result = add(x, y)  # Calling the add_numbers function with user inputs
     print("The result of adding the two numbers is:", result)  # Displaying the result
 
 # Test case class for the calculator functionality
@@ -17,11 +17,11 @@ class TestCalculator(unittest.TestCase):
     # Test method to ensure addition works correctly
     def test_addition(self):
         # Test case 1: Adding 2 and 3 should be 5
-        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add(3, 3), 6)
         # Test case 2: Adding -1 and 5 should be 4
-        self.assertEqual(add_numbers(-1, 5), 4)
+        self.assertEqual(add(-1, 10), 9)
         # Test case 3: Adding 0 and 0 should be 0
-        self.assertEqual(add_numbers(0, 0), 0)
+        self.assertEqual(add(0, 2), 2)
 
 # Checking if the script is run directly
 if __name__ == '__main__':
